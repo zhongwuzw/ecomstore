4b4cb2d54e1acfd4d50469aa8a265f784f5325e1
zhongwuzw <frank@Franks-MacBook-Pro.local>
2014-06-03 23:50:33 +0800

    edit

--- ecomstore/urls.py
+++ ecomstore/urls.py
@@ -7,6 +7,5 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ecomstore.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    
     url(r'^admin/', include(admin.site.urls)),
 )
