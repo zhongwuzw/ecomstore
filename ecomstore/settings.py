eec94c7f8713166fb69749523d26cff0e169d020
zhongwuzw <zhongwuzw@qq.com>
2014-06-04 23:22:37 +0800

    edit

--- ecomstore/settings.py
+++ ecomstore/settings.py
@@ -86,6 +86,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
                     '/Users/frank/Documents/workspace/ecomstore/static',
                     )
